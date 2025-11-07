bd75cef880689db7c3162a2333532e5d90b4afd2
parro <hackingtest017@gmail.com>
2025-11-07 18:38:32 +0300

    integrate session hook

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { ReactNode } from "react";
 import { MainLayout } from "@/layouts/MainLayout";
+import { useSyncSession } from "@/hooks/useSyncSession";
 
 
 export default function AppLayout({ children }: { children: ReactNode }) {
+    useSyncSession();
     return (
         <MainLayout>
             {children}
