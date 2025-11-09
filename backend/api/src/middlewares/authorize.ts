f7df11c5b4fcd29e14b129a16d0adaea6366d74d
parro <beabwuletaw@gmail.com>
2025-11-09 10:19:23 +0300

    support single string parameter

--- backend/api/src/middlewares/authorize.ts
+++ backend/api/src/middlewares/authorize.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
-export function authorize(allowedRoles: string[]) {
+export function authorize(allowedRoles: string[] | string) {
     return (req: Request & { user?: { role?: string } }, res: Response, next: NextFunction) => {
         const userRole = req.user?.role;
-        if (userRole && allowedRoles.includes(userRole)) {
+        if (userRole && (Array.isArray(allowedRoles) ? allowedRoles.includes(userRole) : allowedRoles === userRole)) {
             next();
         } else {
             res.status(403).json({ message: "Access denied." });
