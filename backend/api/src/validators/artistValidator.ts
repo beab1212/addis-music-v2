cf20bd2ab8bf0b8b8a7dc136930d6ecf6285ec82
parro <beabwuletaw@gmail.com>
2025-11-09 13:26:03 +0300

    add artist validator

---
+++ backend/api/src/validators/artistValidator.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const createArtistSchema = z.object({
+    name: z.string()
+    .min(1, "Name is required")
+    .max(50, "Name must be at most 50 characters"),
+    bio: z.string()
+    .min(5, "Bio must be at least 5 characters")
+    .max(500, "Bio must be at most 500 characters")
+    .optional(),
+    file: z.any().optional(),
+});
