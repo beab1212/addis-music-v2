681fa9ee0640f9af3691a382ea81641779f7b0ce
parro <beabwuletaw@gmail.com>
2025-11-09 12:01:36 +0300

    integrate new error handling method

--- backend/api/src/controllers/userController.ts
+++ backend/api/src/controllers/userController.ts
@@ -1,45 +1,35 @@
 import type { Request, Response } from 'express';
+import { CustomErrors } from '../errors';
 import prisma from '../libs/db';
-import { success } from 'better-auth/*';
 
 export const userController = {
     getUser: async (req: Request, res: Response) => {
-        try {
-            const userId = req.params.id;
-            const user = await prisma.user.findUnique({
-                where: { id: userId },
-            });
+        const userId = req.params.id;
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+        });
 
-            if (!user) {
-                return res.status(404).json({ message: 'User not found' });
-            }
-
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ message: 'Internal server error', error });
+        if (!user) {
+            throw new CustomErrors.NotFoundError('User not found');
         }
+
+        res.status(200).json({ success: true, data: { user } });
     },
-    getMe: async (req: Request, res: Response) => {
-        try {   
-            console.log("Current Session User: ", req.user);     
-            const userId = req.user?.id;
-            console.log("Current Session User: ", req.user);
+    getMe: async (req: Request, res: Response) => {        
+        const userId = req.user?.id;
 
-            if (!userId) {
-                return res.status(401).json({ success: false, message: 'Unauthorized' });
-            }
-
-            const user = await prisma.user.findUnique({
-                where: { id: userId },
-            });
-
-            if (!user) {
-                return res.status(404).json({ success: false, message: 'User not found' });
-            }
-
-            res.status(200).json({ success: true, data: { user } });
-        } catch (error) {
-            res.status(500).json({ success: false, message: 'Internal server error', error });
+        if (!userId) {
+            throw new CustomErrors.UnauthenticatedError('Unauthorized');
         }
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+        });
+
+        if (!user) {
+            throw new CustomErrors.NotFoundError('User not found');
+        }
+
+        res.status(200).json({ success: true, data: { user } });
     },
 }
