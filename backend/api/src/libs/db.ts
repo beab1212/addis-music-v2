0411feb093ab8ccc7146350c18694d06a47bc3df
parro <hackingtest017@gmail.com>
2025-11-10 16:41:13 +0300

    add db connection checker

--- backend/api/src/libs/db.ts
+++ backend/api/src/libs/db.ts
@@ -2,4 +2,15 @@ import { PrismaClient } from '@prisma/client'
 
 let prisma = new PrismaClient()
 
+const verifyDatabaseConnection = async () => {
+  try {
+    await prisma.$connect();
+        console.log('Database connection established');
+    } catch (error) {
+        console.error('Database connection failed:', error);
+    }
+}
+
+verifyDatabaseConnection();
+
 export default prisma
