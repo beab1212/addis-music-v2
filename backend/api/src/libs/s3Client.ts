ce2565455e5446c23fe37149ac559cf445f641b3
parro <hackingtest017@gmail.com>
2025-11-17 14:43:47 +0300

    feat: implement audio upload and delete functions for S3 integration

--- backend/api/src/libs/s3Client.ts
+++ backend/api/src/libs/s3Client.ts
@@ -1,5 +1,7 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import fs from "fs";
 import config from "../config/config";
+import  { v4 as uuidv4 } from "uuid";
 
 export const s3Client = new S3Client({
     region: config.s3Storage.region,
@@ -10,3 +12,46 @@ export const s3Client = new S3Client({
     },
     forcePathStyle: true,       // IMPORTANT for MinIO compatibility
 });
+
+export const uploadAudioToS3 = async (filePath: string, isAdd: boolean = false) => {
+    const fileStream = fs.createReadStream(filePath);
+
+    const path = isAdd ? 'add' : 'music';
+
+    const uploadParams = {
+        Bucket: config.s3Storage.bucketName,
+        Key: `${path}/${filePath.split('/').pop()}`, // use multer generated file name
+        Body: fileStream,
+        ContentType: 'audio/mp3', // adjust based on your audio type
+    };
+
+    try {
+        const data = await s3Client.send(new PutObjectCommand(uploadParams));
+        // delete the file locally after upload if needed
+        fs.unlinkSync(filePath);
+        return `${config.s3Storage.endpoint}/${config.s3Storage.bucketName}/${uploadParams.Key}`;
+    } catch (err) {
+        console.error("Error uploading audio to S3:", err);
+        throw err;
+    }
+}
+
+export const generatePresignedUrl = (key: string, expiresIn: number = 3600) => {
+    const url = `${config.s3Storage.endpoint}/${config.s3Storage.bucketName}/${key}?expires_in=${expiresIn}&token=${uuidv4()}`;
+    return url;
+}
+
+export const deleteAudioFromS3 = async (key: string) => {
+    const deleteParams = {
+        Bucket: config.s3Storage.bucketName,
+        Key: key,
+    };
+
+    try {
+        await s3Client.send(new DeleteObjectCommand(deleteParams));
+    } catch (err) {
+        console.error("Error deleting audio from S3:", err);
+        throw err;
+    }
+}
+
