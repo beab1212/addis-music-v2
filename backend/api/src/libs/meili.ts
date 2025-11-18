6c96070a647d5f9ac97ced152c0ba19f7c1783c1
parro <beabwuletaw@gmail.com>
2025-11-18 17:34:50 +0300

    feat: add track indexing to MeiliSearch and update track validation schema

--- backend/api/src/libs/meili.ts
+++ backend/api/src/libs/meili.ts
@@ -1,5 +1,6 @@
 import { MeiliSearch } from "meilisearch";
 import config from "../config/config";
+import prisma from "./db";
 
 
 const meiliClient = new MeiliSearch({
@@ -17,5 +18,41 @@ export const checkMeiliConnection = async () => {
     console.error("Error connecting to MeiliSearch:", error);
   }
 };
+
+
+export const addTrackToMeiliIndex = async (trackId: string) => {
+  try {
+    const index = meiliClient.index('tracks');
+    const track = await prisma.track.findUnique({
+      where: { id: trackId },
+      include: {
+        artist: true,
+        album: true,
+        genre: true,
+      },
+    });
+
+    if (!track) {
+      return null;
+    }
+
+    const trackData = {
+      id: track.id,
+      title: track.title,
+      artist: track.artist.name,
+      album: track.album ? track.album.title : null,
+      genre: track.genre ? track.genre.name : null,
+      releaseDate: track.releaseDate,
+      tags: track.tags,
+      description: track.description,
+    };
+
+    const response = await index.addDocuments([trackData]);
+    console.log("Track added to MeiliSearch index:", response);
+  } catch (error) {
+    console.error("Error adding track to MeiliSearch index:", error);
+  }
+}
+
 
 export default meiliClient;
--- backend/api/src/validators/trackValidator.ts
+++ backend/api/src/validators/trackValidator.ts
@@ -8,4 +8,13 @@ export const uploadTrackSchema = z.object({
     tags: z.array(
         z.string().min(1, "Tag must be min 1 character").max(10, "Tag must be max 10 characters")
     ).max(10, "Maximum of 10 tags allowed").optional(),
+    releaseDate: z
+        .string()
+        .min(1, "Release date is required") // ensures field is not empty
+        .refine((val) => !isNaN(Date.parse(val)), {
+          message: "Release date must be a valid date",
+        })
+        .transform((val) => new Date(val)).optional(),
+    description: z.string().max(500, "Description must be at most 500 characters").optional(),
+    credit: z.string().max(300, "Credit must be at most 300 characters").optional(),
 });
