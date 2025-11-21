7de31e2246a84790759d6327c50fb063be77e222
parro <hackingtest017@gmail.com>
2025-11-21 20:48:52 +0300

    feat: implement session check in AppLayout to redirect unauthenticated users

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -3,9 +3,18 @@ import { ReactNode } from "react";
 import { MainLayout } from "@/layouts/MainLayout";
 import { useSyncSession } from "@/hooks/useSyncSession";
 
+import { authClient } from "@/lib/auth-client";
+import { redirect } from "next/navigation";
+
 
 export default function AppLayout({ children }: { children: ReactNode }) {
+    const { data: session, isPending, error } = authClient.useSession();
     useSyncSession();
+
+    if (!session) {
+        return redirect("/")
+    }
+
     return (
         <MainLayout>
             {children}
