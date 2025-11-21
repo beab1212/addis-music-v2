dab538073a2877cb7110b7edaaafd5f132a95905
parro <beabwuletaw@gmail.com>
2025-11-21 21:36:25 +0300

    feat: wrap children in Protected component for route protection

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -2,22 +2,14 @@
 import { ReactNode } from "react";
 import { MainLayout } from "@/layouts/MainLayout";
 import { useSyncSession } from "@/hooks/useSyncSession";
-
-import { authClient } from "@/lib/auth-client";
-import { redirect } from "next/navigation";
+import Protected from "@/components/Protected"
 
 
 export default function AppLayout({ children }: { children: ReactNode }) {
-    const { data: session, isPending, error } = authClient.useSession();
     useSyncSession();
-
-    if (!session) {
-        return redirect("/")
-    }
-
     return (
         <MainLayout>
-            {children}
+            <Protected>{children}</Protected>
         </MainLayout>
     );
 }
