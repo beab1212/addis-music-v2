928c6f52d4ed2d46e0ea7548fe783386061d782d
parro <hackingtest017@gmail.com>
2025-11-25 11:18:17 +0300

    fix: return an empty array if no data is found in signed URL response

--- backend/api/src/utils/generateSignedUrl.ts
+++ backend/api/src/utils/generateSignedUrl.ts
@@ -9,7 +9,7 @@ export const generateSignedUrl = async (audioId: string, isAdd: boolean = false,
             audio_id: audioId, 
             is_add: isAdd, 
             expiration: expiresInSeconds } });
-        return response.data;
+        return response.data?.data || [];
     } catch(e) {
         console.error('Error generating signed URL', e);
         throw new Error('Error generating signed URL');
