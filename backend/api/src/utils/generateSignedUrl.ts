9ee4c8a5cee72614f8707c39e0f22e3ca05bde3a
parro <beabwuletaw@gmail.com>
2025-11-25 10:59:32 +0300

    feat: implement generateSignedUrl function for presigned URL generation

---
+++ backend/api/src/utils/generateSignedUrl.ts
@@ -0,0 +1,17 @@
+import { mediaServer } from "../libs/axios";
+
+
+export const generateSignedUrl = async (audioId: string, isAdd: boolean = false, expiresInSeconds?: number) => {
+    
+    // Generate the presigned URL
+    try {
+        const response = await mediaServer.get('/generatePresignedUrl', { params: { 
+            audio_id: audioId, 
+            is_add: isAdd, 
+            expiration: expiresInSeconds } });
+        return response.data;
+    } catch(e) {
+        console.error('Error generating signed URL', e);
+        throw new Error('Error generating signed URL');
+    }
+}
