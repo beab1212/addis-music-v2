6c0bf20018e6ec88fe921b1495d99d9083915fee
parro <hackingtest017@gmail.com>
2025-11-29 11:26:50 +0300

    refactor: remove unused recommendation server axios instance

--- backend/api/src/libs/axios.ts
+++ backend/api/src/libs/axios.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 
 const MEDIA_SERVER_BASE_URL = process.env.MEDIA_SERVER_BASE_URL || 'http://localhost:8000';
-const RECOMMENDATION_SERVER_BASE_URL = process.env.RECOMMENDATION_SERVER_BASE_URL || 'http://localhost:8001';
 
 const mediaServer: AxiosInstance = axios.create({
     baseURL: MEDIA_SERVER_BASE_URL,
@@ -20,22 +19,4 @@ mediaServer.interceptors.response.use(
     }
 );
 
-const recommendationServer: AxiosInstance = axios.create({
-    baseURL: RECOMMENDATION_SERVER_BASE_URL,
-    timeout: 15_000,
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
-});
-
-recommendationServer.interceptors.response.use(
-    (response: AxiosResponse) => response,
-    (error: AxiosError) => {
-        // You can add custom error handling logic here
-        return Promise.reject(error);
-    }
-);
-
-
-export { mediaServer, recommendationServer };
+export { mediaServer };
