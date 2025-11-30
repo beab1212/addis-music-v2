847b675db60f037593ca11d036a094ed827b688b
parro <hackingtest017@gmail.com>
2025-11-30 13:32:02 +0300

    feat: add HLSPlayer component to AppLayout for enhanced audio playback

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -2,13 +2,16 @@
 import { ReactNode } from "react";
 import { MainLayout } from "@/layouts/MainLayout";
 import { useSyncSession } from "@/hooks/useSyncSession";
-import Protected from "@/components/Protected"
+import Protected from "@/components/Protected";
+import { HLSPlayer } from "@/components/HLSPlayer";
 
 
 export default function AppLayout({ children }: { children: ReactNode }) {
     useSyncSession();
     return (
         <MainLayout>
+            {/* Add HLSPlayer for playback */}
+            <HLSPlayer />
             <Protected>{children}</Protected>
         </MainLayout>
     );
