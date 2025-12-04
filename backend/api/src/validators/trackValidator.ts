bb8a2c3c2e399693d282e512279a91833cb81b31
parro <beabwuletaw@gmail.com>
2025-12-04 14:30:09 +0300

    fix: update albumId to be nullable and ensure optional fields can accept null values

--- backend/api/src/validators/trackValidator.ts
+++ backend/api/src/validators/trackValidator.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const uploadTrackSchema = z.object({
     title: z.string().min(1, "Title is required").max(100, "Title is too long"),
     artistId: z.string().uuid("Artist ID must be a valid UUID"),
-    albumId: z.string().optional(),
+    albumId: z.string().nullable().optional(),
     genreId: z.string().uuid("Genre Id must be a valid UUID"),
     tags: z.array(
         z.string().min(1, "Tag must be min 1 character").max(10, "Tag must be max 10 characters")
@@ -14,7 +14,7 @@ export const uploadTrackSchema = z.object({
         .refine((val) => !isNaN(Date.parse(val)), {
           message: "Release date must be a valid date",
         })
-        .transform((val) => new Date(val)).optional(),
-    description: z.string().max(500, "Description must be at most 500 characters").optional(),
-    credit: z.string().max(300, "Credit must be at most 300 characters").optional(),
+        .transform((val) => new Date(val)).optional().nullable(),
+    description: z.string().max(500, "Description must be at most 500 characters").optional().nullable(),
+    credit: z.string().max(300, "Credit must be at most 300 characters").optional().nullable(),
 });
