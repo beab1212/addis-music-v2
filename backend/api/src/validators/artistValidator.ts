7006beb9dc6b3681be59c917ca4105f3e882a199
parro <beabwuletaw@gmail.com>
2025-12-05 13:24:48 +0300

    fix: refactor artistValidator to improve isVerified handling and formatting

--- backend/api/src/validators/artistValidator.ts
+++ backend/api/src/validators/artistValidator.ts
@@ -2,14 +2,18 @@ import { z } from "zod";
 
 export const createArtistSchema = z.object({
     name: z.string()
-    .min(1, "Name is required")
-    .max(50, "Name must be at most 50 characters"),
+        .min(1, "Name is required")
+        .max(50, "Name must be at most 50 characters"),
     bio: z.string()
-    .min(5, "Bio must be at least 5 characters")
-    .max(500, "Bio must be at most 500 characters")
-    .optional().nullable(),
-    isVerified: z.boolean().optional().nullable(),
-    
+        .min(5, "Bio must be at least 5 characters")
+        .max(500, "Bio must be at most 500 characters")
+        .optional().nullable(),
+    isVerified: z
+        .union([z.literal("true"), z.literal("false")])  // Allowing "true" and "false" strings
+        .transform(val => val === "true" ? true : val === "false" ? false : null) // Coerce the value to boolean or null
+        .optional()
+        .nullable(),
+
     genres: z.array(z.string().min(2, "Genre must be at least 2 characters").max(30, "Genre must be at most 30 characters")).max(5, "Genres must be at most 5").optional().nullable(),
     file: z.any().optional(),
 });
