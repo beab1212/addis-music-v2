b7159220b1feebb4cf6b3614cdba612b0d53ed06
parro <hackingtest017@gmail.com>
2025-12-18 22:30:00 +0300

    feat: update stream routes to use mainStream and addStream for improved audio streaming

--- backend/api/src/routes/streamRoutes.ts
+++ backend/api/src/routes/streamRoutes.ts
@@ -6,6 +6,8 @@ import { streamController } from "../controllers/streamController";
 const router = Router();
 
 // router.get("/:audioId/master.m3u8", requireAuth, streamController.stream);
-router.get("/:audioId/master.m3u8", requireAuth, streamController.stream);
+router.get("/:audioId/master.m3u8", requireAuth, streamController.mainStream);
+router.get("/:audioId/ads/ad.m3u8", requireAuth, streamController.addStream);
+router.get("/:audioId/stream-url", requireAuth, streamController.stream);
 
 export default router;
