3c745f00f1e66adda378c27aceb775e981ec08ca
parro <beabwuletaw@gmail.com>
2025-12-18 15:14:01 +0300

    feat: add validation schema for multiple track addition to playlist

--- backend/api/src/validators/playlistItemValidator.ts
+++ backend/api/src/validators/playlistItemValidator.ts
@@ -8,5 +8,9 @@ export const addItemToPlaylistSchema = z.object({
     .max(100, "Position must be less than 100 since playlist items are limited to 100").optional(),
 });
 
+export const addIMultipleTrackPlaylistSchema = z.object({
+    trackIds: z.array(z.string().uuid("Invalid track ID")).min(1, "At least one track ID must be provided").max(20, "Cannot add more than 20 tracks at once"),
+});
+
 export const playlistUuidSchema = z.string().uuid("Invalid playlist ID format");
 
